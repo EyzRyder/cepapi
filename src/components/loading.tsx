61544a567065f749de4872ca9863231b812440dc
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-06-06 16:23:22 -0300

    added loader animation

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,7 +1,9 @@
+import Loader from 'react-loaders'
+
 export const Loading = () => {
   return (
-    <div className="absolute top-1 left-1">
-      loading
+    <div className="absolute top-[5%] translate-x-0 -translate-y-[50%]">
+      <Loader active type="ball-pulse-sync" />
     </div>
   )
 }
